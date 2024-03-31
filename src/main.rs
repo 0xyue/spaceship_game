ef93dba1abef9a34ccdde03083c2bc61f208b24b
0xyue <0xyue.eth@gmail.com>
2024-03-31 23:42:01 +0800

    on 0.13 and here's the one thing that changed: AmbientLight now needs to have a brightness of 750.0 instead of 0.75 for the same effect

--- src/main.rs
+++ src/main.rs
@@ -13,10 +13,10 @@ fn main() {
     App::new()
         // 插入一个 `ClearColor` 资源，设置清除颜色为 RGB(0.1, 0.0, 0.15)。这是每次清除帧缓冲区时使用的颜色。
         .insert_resource(ClearColor(Color::rgb(0.1, 0.0, 0.15)))
-        // 插入一个 `AmbientLight` 资源，设置环境光的颜色为默认颜色，亮度为 0.75。环境光是在整个场景中均匀分布的光源，它影响到场景中所有物体的颜色和亮度。
+        // 插入一个 `AmbientLight` 资源，设置环境光的颜色为默认颜色，亮度为 750.0。环境光是在整个场景中均匀分布的光源，它影响到场景中所有物体的颜色和亮度。
         .insert_resource(AmbientLight {
             color: Color::default(),
-            brightness: 0.75,
+            brightness: 750.0,
         })
         .add_plugins(DefaultPlugins)
         // User defined plugins.
